3bc1b329058789c9c54d0da93577dcab04d35d6b
Mandeep Shergill <mandeeps@users.noreply.github.com>
2014-02-24 09:47:36 -0500

    following along on vogella live wallpaper tutorial

--- src/illusivesun/wallpaper.java
+++ src/illusivesun/wallpaper.java
@@ -2,14 +2,17 @@ package illusivesun;
 
 import java.util.ArrayList;
 
+import android.graphics.Color;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 
+// http://www.vogella.com/tutorials/AndroidLiveWallpaper/article.html
 public class wallpaper extends WallpaperService {
 
 	@Override
 	public Engine onCreateEngine() {
-		// TODO Auto-generated method stub
 		return new wallEngine();
 	}
 	
@@ -31,9 +34,39 @@ public class wallpaper extends WallpaperService {
 		private boolean touchOn;
 		
 		public wallEngine() {
+			maxNum = 20;
+			touchOn = true;
 			circles = new ArrayList<MyPoint>();
+			paint.setAntiAlias(true);
+			paint.setColor(Color.RED);
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setStrokeJoin(Paint.Join.ROUND);
+			paint.setStrokeWidth(10f);
+			handler.post(drawRunner);
 		}
 		
+		@Override
+		public void onVisibilityChanged(boolean visibile) {
+			this.visible = visibile;
+			if (visibile) {
+				handler.post(drawRun);
+			} else {
+				handler.removeCallbacks(drawRun);
+			}
+		}
+	}
+	
+	@Override
+	public void onSurfaceDestroyed(SurfaceHolder holder) {
+		super.onSurfaceDestroyed(holder);
+		this.visible = false;
+		handler.removeCallbacks(drawRun);
+	}
+	
+	@Override void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		this.width = width;
+		this.height = height;
+		super.onSurfaceChanged(holder, format, width, height);
 	}
 
 }
